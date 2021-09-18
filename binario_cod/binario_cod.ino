52a0742dd7823bed0cc6b836dbe74f2b241fc3e1
Gustavo Pontes <69886395+GustavoPontess@users.noreply.github.com>
2021-09-18 00:53:20 -0300

    Update binario_cod.ino

--- binario_cod/binario_cod.ino
+++ binario_cod/binario_cod.ino
@@ -25,7 +25,7 @@ int de[]={65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,8
 const char cha[]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X'};
 void loop() 
 {
-  if(led<30)
+  if(led<30)//faz o led biscar começando pelo que esta no pino 22
   { 
     digitalWrite(led, HIGH);
     delay(100); // Wait for 100 millisecond(s)
